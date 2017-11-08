381f3590b13f53ab064fdfd55b1ea2b505b45080
andrew <uandrew1965@gmail.com>
2017-11-08 17:37:39 +0300

    .

--- src/main/scala/com/simplesys/app/StartPage.scala
+++ src/main/scala/com/simplesys/app/StartPage.scala
@@ -1,20 +1,21 @@
 package com.simplesys.app
 
+import scala.scalajs.js.annotation.JSExportTopLevel
 import scalatags.Text
 import scalatags.Text.all._
 import scalatags.Text.tags2
 
-class StartPage(_title: String) {
+@JSExportTopLevel("GanttImprovedTest")
+object GanttImprovedTest {
     def bodyHTML(lastScript: String): Text.TypedTag[String] = {
-        html(
-            head(
-                tags2.title(_title),
-                meta(httpEquiv := "Content-Type", content := "text/html; charset=utf-8")
+        div(
+            div(
+                style := "position:relative",
+                `class` := "gantt",
+                id := "GanttChartDIV"
             ),
-            body(
-                link(href := "jsgantt-improved/src/main/css/jsgantt.css", rel := "stylesheet", `type` := "text/css"),
-                script(src := "jsgantt-improved/src/main/javascript/jsgantt.js"),
-                script(lastScript)
+            script(
+
             )
         )
     }
