06b1446c528de65d612ef5abda84f9ffc84e77f6
andrew <uandrew1965@gmail.com>
2017-11-25 16:29:40 +0300

    .

--- ganttImproved/.jvm/src/main/scala/com/simplesys/gantt/TaskItemExt.scala
+++ ganttImproved/.jvm/src/main/scala/com/simplesys/gantt/TaskItemExt.scala
@@ -1,26 +1,23 @@
 package com.simplesys.gantt
 
-import java.time.LocalDateTime
-
 import com.simplesys.gantt.Group.Group
 import com.simplesys.gantt.MileStone.MileStone
 import com.simplesys.gantt.Opening.Opening
-import com.simplesys.gantt.TaskCssClass.TaskCssClass
 
 case class TaskItemExt(
-                        pID: Option[Int] = None,
-                        pName: Option[String] = None,
-                        pStart: Option[LocalDateTime] = None,
-                        pEnd: Option[LocalDateTime] = None,
-                        pClass: Option[TaskCssClass] = None,
-                        pLink: Option[Link] = None,
-                        pMile: Option[MileStone] = None,
-                        pRes: Option[String] = None,
-                        pComp: Option[Int] = None,
-                        pGroup: Option[Group] = None,
-                        pParentID: Option[Int] = None,
-                        pOpen: Option[Opening] = None,
-                        pDepend: Option[Seq[Depend]] = None,
-                        pCaption: Option[String] = None,
-                        pNotes: Option[String] = None
-                 )
+                        pID: Int,
+                        pName: String,
+                        pStart: String = "",
+                        pEnd: String = "",
+                        pClass: String,
+                        pLink: String = "",
+                        pMile: MileStone = MileStone.notMilestone,
+                        pRes: String = "",
+                        pComp: Int = 0,
+                        pGroup: Group = Group.normalTask,
+                        pParent: Int = 0,
+                        pOpen: Opening = Opening.open,
+                        pDepend: String = "",
+                        pCaption: String = "",
+                        pNotes: String = ""
+                      )
